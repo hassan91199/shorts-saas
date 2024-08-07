0a58a5157dc4815e49ad2448b0167754ae9868d0
Hassan <hassanabbas4745@gmail.com>
2024-08-07 18:44:56 +0500

    fix: revert back the input error component

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,14 +1,3 @@
-<!-- @props(['messages'])
-
-@if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
-        @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
-        @endforeach
-    </ul>
-@endif -->
-
-
 @props(['messages'])
 
 @if ($messages)
@@ -18,9 +7,3 @@
     @endforeach
 </ul>
 @endif
-
-<div id="validationServer05Feedback" class="invalid-feedback">
-    <ul>
-        <li>Please provide a valid zip.</li>
-    </ul>
-</div>
