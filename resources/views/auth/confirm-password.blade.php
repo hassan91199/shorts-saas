8a92b7b44b800322195a8589e97e8143ea049b8f
Hassan <hassanabbas4745@gmail.com>
2024-08-07 19:18:53 +0500

    feat: make page for confirming the user's password

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,27 +1,14 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+<x-auth-layout>
+    <div class="panel vstack gap-3 w-100 sm:w-350px mx-auto text-center" data-anime="targets: >*; translateY: [24, 0]; opacity: [0, 1]; easing: easeInOutExpo; duration: 750; delay: anime.stagger(100);">
+        <h1 class="h4 sm:h2">{{ __('Confirm Password') }}</h1>
+        <p class="mt-2 sm:m-0">{{ __('This is a secure area of the application. Please confirm your password before continuing.') }}</p>
+        <form method="POST" action="{{ route('password.confirm') }}" class="vstack gap-2">
+            @csrf
+
+            <!-- Password -->
+            <input class="form-control h-48px w-full bg-white dark:bg-opacity-0 dark:text-white dark:border-gray-300 dark:border-opacity-30" type="password" name="password" placeholder="{{ __('Password') }}" required>
+
+            <button class="btn btn-primary btn-md text-white mt-2" type="submit">{{ __('Confirm') }}</button>
+        </form>
     </div>
-
-    <form method="POST" action="{{ route('password.confirm') }}">
-        @csrf
-
-        <!-- Password -->
-        <div>
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <div class="flex justify-end mt-4">
-            <x-primary-button>
-                {{ __('Confirm') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</x-auth-layout>
