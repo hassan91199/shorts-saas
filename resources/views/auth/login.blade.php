8a4fe8b867c84bcffebd194d5602e86f233b4923
Hassan <hassanabbas4745@gmail.com>
2024-08-08 11:33:05 +0500

    fix: show validation errors, session messages, and make the text dyname to local on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,10 @@
 <x-auth-layout>
     <div class="panel vstack gap-3 w-100 sm:w-350px mx-auto text-center" data-anime="targets: >*; translateY: [24, 0]; opacity: [0, 1]; easing: easeInOutExpo; duration: 750; delay: anime.stagger(100);">
-        <h1 class="h4 sm:h2">Sign in</h1>
+        <h1 class="h4 sm:h2">{{ __('Sign in') }}</h1>
+
+        <!-- Session Status -->
+        <x-auth-session-status :status="session('status')" />
+
         <!-- <div class="hstack gap-2">
             <a href="#github" class="hstack items-center justify-center flex-1 gap-1 h-48px text-none rounded bg-dark text-white dark:bg-white dark:text-dark">
                 <i class="icon icon-1 unicon-logo-github"></i>
@@ -15,17 +19,24 @@
         </div> -->
         <form method="POST" action="{{ route('login') }}" class="vstack gap-2">
             @csrf
-            <input class="form-control h-48px w-full bg-white dark:bg-opacity-0 dark:text-white dark:border-gray-300 dark:border-opacity-30" type="email" name="email" placeholder="Email" required>
-            <input class="form-control h-48px w-full bg-white dark:bg-opacity-0 dark:text-white dark:border-gray-300 dark:border-opacity-30" type="password" name="password" placeholder="Password" required>
+
+            <!-- Email Address -->
+            <input class="form-control h-48px w-full bg-white dark:bg-opacity-0 dark:text-white dark:border-gray-300 dark:border-opacity-30" type="email" name="email" value="{{ old('email') }}" placeholder="{{ __('Email') }}" required>
+            <x-input-error :messages="$errors->get('email')" />
+
+            <!-- Password -->
+            <input class="form-control h-48px w-full bg-white dark:bg-opacity-0 dark:text-white dark:border-gray-300 dark:border-opacity-30" type="password" name="password" placeholder="{{ __('Password') }}" required>
+            <x-input-error :messages="$errors->get('password')" />
+
             <div class="hstack justify-between text-start">
                 <div class="form-check text-start rtl:text-end">
                     <input id="uc_form_remember_me" class="form-check-input rounded bg-white dark:bg-opacity-0 dark:text-white dark:border-gray-300 dark:border-opacity-30" type="checkbox" name="remember">
-                    <label for="uc_form_remember_me" class="hstack justify-between form-check-label fs-6">Remember me?</label>
+                    <label for="uc_form_remember_me" class="hstack justify-between form-check-label fs-6">{{ __('Remember me?') }}</label>
                 </div>
-                <a href="{{ route('password.request') }}" class="uc-link">Forgot password</a>
+                <a href="{{ route('password.request') }}" class="uc-link">{{ __('Forgot password') }}</a>
             </div>
-            <button class="btn btn-primary btn-md text-white mt-2" type="submit">Log in</button>
+            <button class="btn btn-primary btn-md text-white mt-2" type="submit">{{ __('Log in') }}</button>
         </form>
-        <p>Have no account yet? <a class="uc-link" href="{{ route('register') }}">Sign up</a></p>
+        <p>{{ __('Have no account yet?') }} <a class="uc-link" href="{{ route('register') }}">{{ __('Sign up') }}</a></p>
     </div>
 </x-auth-layout>
