245e595ce92ceb6d5f97b7fa738cd1892fe49939
Hassan <hassanabbas4745@gmail.com>
2024-08-13 11:41:27 +0500

    feat: make the page for creating the series

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -20,11 +20,11 @@
                         <div class="panel">
                             <div>
                                 <form class="vstack gap-2 p-3 sm:p-6 xl:p-8">
-                                    <div>
+                                    <div id="set-destination-div">
                                         <h2 class="h3 m-0">{{ __('Step 1 - Destination') }}</h2>
                                         <p class="fs-6 text-dark dark:text-white text-opacity-70">{{ __('The account where your video series will be posted') }}</p>
 
-                                        <select class="form-control bg-white dark:border-white dark:bg-opacity-10 dark:border-opacity-0 dark:text-white">
+                                        <select id="set-destination-select" class="form-control bg-white dark:border-white dark:bg-opacity-10 dark:border-opacity-0 dark:text-white">
                                             <option value="" disabled selected>Select an option</option>
                                             <option value="email">Email Me Instead</option>
                                             <option value="tiktok">Link a Tik Tok Account +</option>
@@ -32,11 +32,11 @@
                                         </select>
                                     </div>
 
-                                    <div class="mt-2">
+                                    <div id="set-content-div" class="mt-2 d-none">
                                         <h2 class="h3 m-0">{{ __('Step 2 - Content') }}</h2>
                                         <p class="fs-6 text-dark dark:text-white text-opacity-70">{{ __('What will your video series be about?') }}</p>
 
-                                        <select class="form-control bg-white dark:border-white dark:bg-opacity-10 dark:border-opacity-0 dark:text-white">
+                                        <select id="set-content-select" class="form-control bg-white dark:border-white dark:bg-opacity-10 dark:border-opacity-0 dark:text-white">
                                             <option value="" disabled selected>Select an option</option>
                                             <option>Random AI Story</option>
                                             <option>Scary Stories</option>
@@ -54,7 +54,7 @@
                                         </select>
                                     </div>
 
-                                    <div class="mt-2">
+                                    <div id="create-video-div" class="mt-2 d-none">
                                         <h2 class="h3 m-0">{{ __('Step 3 - Create') }}</h2>
                                         <p class="fs-6 text-dark dark:text-white text-opacity-70">{{ __('You will be able to preview your upcoming videos before posting') }}</p>
                                         <button class="btn btn-primary text-white mt-1" type="submit">{{ __('Create Series +') }}</button>
@@ -69,4 +69,18 @@
     </div>
 
     <!-- Section end -->
+
+    <x-slot name="script">
+        <script>
+            $(document).ready(function() {
+                $('#set-destination-select').change(function() {
+                    $('#set-content-div').removeClass('d-none');
+                });
+
+                $('#set-content-select').change(function() {
+                    $('#create-video-div').removeClass('d-none');
+                });
+            });
+        </script>
+    </x-slot>
 </x-app-layout>
