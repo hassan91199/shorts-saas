19bcc5fea12cd9f400bb6da092571e954839fe3a
Hassan <hassanabbas4745@gmail.com>
2024-08-25 12:41:32 +0500

    feat: make listener for uploading video to tiktok

---
+++ app/Listeners/UploadVideoToTikTok.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\VideoRendered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class UploadVideoToTikTok
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(VideoRendered $event): void
+    {
+        $videoSeries = $event->video->series;
+
+        if ($videoSeries->destination === 'tiktok') {
+            Log::info('Catched video rendered event, uploading video to tiktok.');
+        }
+    }
+}
