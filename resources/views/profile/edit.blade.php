7308571e680f9a21d077e8b86d0d01111c2d64b7
Hassan <hassanabbas4745@gmail.com>
2024-09-14 19:41:46 +0500

    feat: integrate the backend for updating the profile info

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,14 +1,30 @@
 <x-app-layout>
     <!-- Section start -->
-    <h2 class="h3 mt-5">{{ __('Update Email') }}</h2>
+    <h2 class="h3 mt-5">{{ __('Profile Information') }}</h2>
     <div class="panel rounded-3 overflow-hidden bg-secondary dark:bg-gray-800 p-3">
-        <form class="vstack gap-1 d-flex flex-row">
-            <input type="email"
+        <form method="post" action="{{ route('profile.update') }}" class="vstack gap-1">
+            @csrf
+            @method('patch')
+
+            <input id="name" name="name" type="text" value="{{ old('name', $user->name) }}"
+                class="form-control form-control-sm w-full fs-6 bg-white dark:border-white dark:border-gray-700"
+                placeholder="Type your name here">
+            <x-input-error :messages="$errors->get('name')" />
+
+            <input id="email" name="email" type="email" value="{{ old('email', $user->email) }}"
                 class="form-control form-control-sm w-full fs-6 bg-white dark:border-white dark:border-gray-700"
                 placeholder="Type your new email here">
-            <button type="submit" class="btn btn-sm btn-primary">Save</button>
+            <x-input-error :messages="$errors->get('email')" />
+            <p class="fs-7 fw-light">{{ __('Note: This will be your login email. Make sure to remember it!') }}</p>
+
+            <div class="text-end mt-1">
+                @if (session('status') === 'profile-updated')
+                <span id="saved-message" class="fs-6 text-primary">{{ __('Saved!') }}</span>
+                @endif
+
+                <button type="submit" class="btn btn-sm btn-primary">Save</button>
+            </div>
         </form>
-        <p class="fs-7 fw-light mt-2">{{ __('Note: This will be your login email. Make sure to remember it!') }}</p>
     </div>
     <!-- Section end -->
 
@@ -37,7 +53,7 @@
     <div class="panel rounded-3 overflow-hidden bg-secondary dark:bg-gray-800 p-3">
         <form class="vstack gap-1">
             <div id="" class="form-group vstack gap-1">
-                <label class="fs-5 fw-medium form-label" for="set-account-select">{{ __('Account') }} <i class="unicon-information" title="Select where you want the videos in your series to be uploaded or sent"></i></label>
+                <label class="fs-5 fw-medium form-label" for="set-account-select">{{ __('Account') }} <i class="unicon-information" data-uc-tooltip="Select where you want the videos in your series to be uploaded or sent"></i></label>
                 <select id="set-account-select" class="form-select form-control-sm rounded dark:bg-gray-100 dark:bg-opacity-5 dark:text-white dark:border-gray-800" name="" aria-label="" required>
                     <option value="" disabled selected>{{__('Select account')}}</option>
                     <!-- <option value="email">{{__('Email Me Instead')}}</option> -->
@@ -45,7 +61,7 @@
             </div>
 
             <div id="" class="form-group vstack gap-1">
-                <label class="fs-5 fw-medium form-label" for="description-footer-textarea">{{ __('Description Footer') }} <i class="unicon-information" title="This text will be added to the end of the description of every video uploaded to this account."></i></label>
+                <label class="fs-5 fw-medium form-label" for="description-footer-textarea">{{ __('Description Footer') }} <i class="unicon-information" data-uc-tooltip="This text will be added to the end of the description of every video uploaded to this account."></i></label>
                 <textarea for="description-footer-textarea" class="form-control min-h-100px h-auto w-full bg-white dark:border-white dark:text-dark" placeholder="Appears at the end of every video description / caption. 
                 
 Example: &quot;Subscribe to our channel for more videos!&quot;"></textarea>
@@ -62,8 +78,11 @@ Example: &quot;Subscribe to our channel for more videos!&quot;"></textarea>
 
     <x-slot name="script">
         <script>
-            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-            const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+            document.addEventListener("DOMContentLoaded", function() {
+                setTimeout(function() {
+                    document.getElementById('saved-message').style.display = 'none';
+                }, 3000);
+            });
         </script>
     </x-slot>
 </x-app-layout>
